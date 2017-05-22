1bf30c99b1c72a526459d9e00d6da1d6f7b2eac2
Marco Schlicht <marcoschlicht@onlinehome.de>
2017-05-22 18:43:31 +0200

    F pointers and references bis Aufgabe 15

--- CPP_VM/F_pointers_and_references/F12_function_pointers.cpp
+++ CPP_VM/F_pointers_and_references/F12_function_pointers.cpp
@@ -21,41 +21,41 @@ double negsin(double d) { return -sin(d); }
 
 int main()
 {
-    double d = 3.14;
-    func_t f = sin;   // das ist ein Funktionszeiger init. mit 'sin'
-    
-    tools_log(); cout << "sin(" << d << ")=" << sin(d) << endl;
-    tools_log(); cout << "  f(" << d << ")=" << f(d) << endl;
-    // strenggenommen ist f ja ein Zeiger, daher ist folgendes mgl.
-    tools_log(); cout << "  f(" << d << ")=" << (*f)(d) << endl;
-    
-//    f = negsin;
+	double d = 3.14;
+	func_t f = sin;   // das ist ein Funktionszeiger init. mit 'sin'
 
-    // so sieht die Definition aus, wenn kein typedef verwendet wird
-    double (*g)(double) = sin;    
-    tools_log(); cout << "  g(" << d << ")=" << g(d) << endl;
+	tools_log(); cout << "sin(" << d << ")=" << sin(d) << endl;
+	tools_log(); cout << "  f(" << d << ")=" << f(d) << endl;
+	// strenggenommen ist f ja ein Zeiger, daher ist folgendes mgl.
+	tools_log(); cout << "  f(" << d << ")=" << (*f)(d) << endl;
 
-    // analog gibt es auch Funktionsreferenzen (nicht so bekannt)
-    double (&h)(double) = sin;    
-    tools_log(); cout << "  h(" << d << ")=" << h(d) << endl << endl;
+	//    f = negsin;
 
-    // was ist das?
-    func_t fa[] = { sin, negsin, cos };
-    // double (*fa[])(double) = { sin, negsin, cos };
-    
-    for (double x=0.0; x<M_PI+0.5; x+=0.5)  // M_PI in cmath
-    {
-        tools_log(); 
-        for (unsigned int i=0; i<sizeof(fa)/sizeof(func_t); ++i)
-        {
-            // etwas formatierung (iomanip, folgt noch)
-            cout << setprecision(2);
-            cout << "fa[" << i << "](" << setw(3) << x 
-                 << ")=" << setw(6) << fa[i](x) << " ";
-        }
-        cout << endl;
-    }
+	// so sieht die Definition aus, wenn kein typedef verwendet wird
+	double (*g)(double) = sin;    
+	tools_log(); cout << "  g(" << d << ")=" << g(d) << endl;
 
-    return EXIT_SUCCESS;
+	// analog gibt es auch Funktionsreferenzen (nicht so bekannt)
+	double (&h)(double) = sin;    
+	tools_log(); cout << "  h(" << d << ")=" << h(d) << endl << endl;
+
+	// was ist das?
+	func_t fa[] = { sin, negsin, cos };
+	// double (*fa[])(double) = { sin, negsin, cos };
+
+	for (double x=0.0; x<M_PI+0.5; x+=0.5)  // M_PI in cmath
+	{
+		tools_log(); 
+		for (unsigned int i=0; i<sizeof(fa)/sizeof(func_t); ++i)
+		{
+			// etwas formatierung (iomanip, folgt noch)
+			cout << setprecision(2);
+			cout << "fa[" << i << "](" << setw(3) << x 
+				<< ")=" << setw(6) << fa[i](x) << " ";
+		}
+		cout << endl;
+	}
+
+	return EXIT_SUCCESS;
 }
 
--- CPP_VM/F_pointers_and_references/F14_varargs.cpp
+++ CPP_VM/F_pointers_and_references/F14_varargs.cpp
@@ -17,10 +17,10 @@ void show_alphabet(int count, ...);
 
 int main()
 {
-    sum_args(1,'A'); // 2,4,8,0);
-    //show_alphabet(3, 'A',65, 'B',66, 'C',67);
+	sum_args(1,'A'); // 2,4,8,0);
+	//show_alphabet(3, 'A',65, 'B',66, 'C',67);
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
 
 // ein Argument muss vor den '...' immer angegeben werden und die Art und Weise
@@ -32,30 +32,30 @@ int main()
 // alles aufsummieren, bis eine 0 als Par. folgt
 void sum_args(int n1, ...)
 {
-    va_list l;
-    va_start(l,n1);         // init list
+	va_list l;
+	va_start(l,n1);         // init list
 
-    int n=n1,sum=0;
-    while(n>0)
-    {
-        tools_log(); cout << "Param=" << n << endl;
-        sum += n;
-        n=va_arg(l,int);    // take next par
-    }
-    tools_log(); cout << "Summe=" << sum << endl << endl;
+	int n=n1,sum=0;
+	while(n>0)
+	{
+		tools_log(); cout << "Param=" << n << endl;
+		sum += n;
+		n=va_arg(l,int);    // take next par
+	}
+	tools_log(); cout << "Summe=" << sum << endl << endl;
 }
 
 // zu Beginn die Anzahl von Paerchen angeben
 void show_alphabet(int count, ...)
 {
-    va_list l;
-    va_start(l,count);
-    
-    for (int i=0; i<count; ++i)
-    {
-        char c = (char)va_arg(l,int);  // char is stored as int!
-        int  n = va_arg(l,int);
-        tools_log(); cout << "Param=(" << c << "," << n << ")" << endl;
-    }
+	va_list l;
+	va_start(l,count);
+
+	for (int i=0; i<count; ++i)
+	{
+		char c = (char)va_arg(l,int);  // char is stored as int!
+		int  n = va_arg(l,int);
+		tools_log(); cout << "Param=(" << c << "," << n << ")" << endl;
+	}
 }
 
