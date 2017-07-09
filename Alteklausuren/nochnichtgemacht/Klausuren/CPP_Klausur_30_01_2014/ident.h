2086ceb4055f2e4bcddbc6528d34c79fa8617b93
Marco Schlicht <marcoschlicht@onlinehome.de>
2017-07-09 23:53:59 +0200

    removed useless copys

--- Alteklausuren/nochnichtgemacht/Klausuren/CPP_Klausur_30_01_2014/ident.h
+++
@@ -1,5 +0,0 @@
-// Prüfung C++, Winter 2013/14, Terstegge/Voss
-
-const char * matse_name   = "Vorname Nachname";
-const char * matse_matrnr = "123456";
-
--- Alteklausuren/nochnichtgemacht/Klausuren/C_C++_Klausur_01.07.2013/c++_haidl_loesungen/ident.h
+++
@@ -1,4 +0,0 @@
-// Prüfung C, Sommer 2013, A.Voss@FH-Aachen.de
-
-const char * matse_name()   { return "Haidl"; }
-const char * matse_matrnr() { return "864333"; }
